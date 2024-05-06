15aecd38d4b16f6eec5e648d98424a2d2e6af60f
Maksim59 <max.isayenka@gmail.com>
2024-05-06 12:12:24 -0400

    made a sound for the elphant

--- Elephant.java
+++ Elephant.java
@@ -8,13 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
     public void act()
     {
-        // Add your action code here.
+        
         if(Greenfoot.isKeyDown("left"))
         {
             move(-1);
@@ -24,7 +21,20 @@ public class Elephant extends Actor
             move(1);
         }
         
-        removeTouching(Apple.class);
+        eat();
         
+    }
+
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+            elephantSound.play();
+            
+        }
     }
 }
