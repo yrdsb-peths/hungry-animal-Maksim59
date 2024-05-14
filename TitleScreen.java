8901b9e10061ee5dac648f708db21a399e495dd3
Maksim59 <max.isayenka@gmail.com>
2024-05-13 20:36:54 -0400

    tried to do a title screen and adding more functions

--- TitleScreen.java
+++ TitleScreen.java
@@ -19,7 +19,7 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, getWidth()/2, 2);
         prepare();
     }
 
@@ -31,13 +31,27 @@ public class TitleScreen extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
     {
-        Elephant elephant = new Elephant();
-        addObject(elephant,528,70);
+        Label label = new Label("Press <-- or --> to move", 40);
+        addObject(label,79,195);
+        label.setLocation(326,200);
+        Label label2 = new Label("Press <space> to start", 40);
+        addObject(label2,149,146);
+        label2.setLocation(249,159);
+        label.setLocation(167,281);
+        label2.setLocation(152,199);
+        label.setLocation(128,268);
+        label2.setLocation(135,225);
     }
 }
